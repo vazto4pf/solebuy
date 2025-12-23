9de34171d0d299e3aa7b854ef8e2044f5091ccb4
vazto4pf <vaztomundo360@gmail.com>
2025-12-23 06:15:47 -0600

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
         <div className="text-center">
           <p className="text-gray-400 text-sm">
-            &copy; {currentYear} SoleBuy || k3Lsoft Devs. All rights reserved.
+            &copy; {currentYear} SoleBuy | k3LSoft Devs. | All rights reserved.
           </p>
         </div>
       </div>
